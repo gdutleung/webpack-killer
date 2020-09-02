a389e515a63a8244608859267b06aaaca0076f66
peiran.liang <peiran.liang@zhenai.com>
2020-09-02 11:17:15 +0800

    feat: 管理资源

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import _ from 'lodash'
+import _ from "lodash";
+import "./style.css";
+import Avatar from "./jay.jpg";
 
 function component() {
-  var element = document.createElement('div');
+    var element = document.createElement("div");
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.classList.add("hello");
+    // 将图像添加到我们现有的 div。
+    var myAvatar = new Image();
+    myAvatar.src = Avatar;
 
-  return element;
+    element.appendChild(myAvatar);
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            webp: {
+                                quality: 5,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
